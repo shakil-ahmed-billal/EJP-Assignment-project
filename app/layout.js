0fe70a30236829c8beecf519d736e0eed81a6453
shakil-ahmed-billal <itzxhakil@gmail.com>
2025-01-23 23:47:30 +0600

    Implement Kinde authentication with AuthProvider and Header component

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
+import { AuthProvider } from "@/provider/AuthProvider";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AuthProvider>
+          <Header></Header>
+          <main>{children}</main>
+        </AuthProvider>
       </body>
     </html>
   );
---
+++ components/header/Header.jsx
@@ -0,0 +1,47 @@
+import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { LogOut } from "lucide-react";
+
+const Header = async () => {
+
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    return (
+        <div className="bg-gray-100 bg-opacity-50 sticky top-0 backdrop-blur-sm">
+            <div className="flex justify-between items-center p-4 w-11/12 mx-auto">
+                <div className="">
+                    <Image
+                        src={"/blog_logo.png"}
+                        alt="blog site logo"
+                        width={80}
+                        height={80}
+                    ></Image>
+                </div>
+                <div className="space-x-2 flex items-center justify-center">
+                    <Link href={"/"}> Home</Link>
+                    <Link href={"/blog"}> Blogs</Link>
+                    <Link href={"/profile"}> Profile</Link>
+                    {user ? (
+                        <div className="">
+                            <LogoutLink>
+                                <Button variant={"destructive"} className="rounded-full"><LogOut /></Button>
+                            </LogoutLink>
+                        </div>
+                    ) : (
+                        <>
+                            <LoginLink>
+                                <Button>Login</Button>
+                            </LoginLink>
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
